268a5cb46b22a5d4a0e430be3cf087ba346f580c
spv00 <sean.pa.voss@gmail.com>
2022-08-13 01:53:51 +0200

    ah yes

--- client.lua
+++ client.lua
@@ -9,11 +9,8 @@ i = 0
 while true do
     oldi = i
     i = tonumber(get("index"))
-    print("i: " .. i)
-    print("oldi: " .. oldi)
     if i < oldi then
         cmd = get("cmd")
-        print(cmd)
         i = i + 1
         if cmd == "forward" then
             turtle.forward()
