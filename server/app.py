ce04b12a0e2d4842adab1a20d74dd9d94a02b3b6
spv00 <sean.pa.voss@gmail.com>
2022-08-13 02:26:09 +0200

    ah yes

--- server/app.py
+++ server/app.py
@@ -6,11 +6,20 @@ i = 1
 
 queue: list = []
 
+cmd = "forward"
+
 @app.route("/increase")
 def increase():
     global i
     i += 1
     return str(i)
+
+@app.route("/setcmd/<newcmd>")
+def setcmd(newcmd):
+    global cmd
+    cmd = newcmd
+    increase()
+    return f"ok - {cmd}"
 
 @app.route("/controls")
 def controls():
@@ -23,8 +32,7 @@ def index():
 
 @app.route("/cmd")
 def test():
-    x = open("action.txt")
-    return str(x.read())
+    return cmd
 
 if __name__ == "__main__":
     app.run()
