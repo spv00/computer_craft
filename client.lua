23440b93d5c3c9bc27a1d880b2664e09623ad167
spv00 <sean.pa.voss@gmail.com>
2022-08-13 02:34:46 +0200

    ah yes

--- client.lua
+++ client.lua
@@ -4,6 +4,10 @@ function get(dir)
     return http.get(url .. dir).readAll()
 end
 
+function resp(dir, data)
+    http.post(url .. dir, data)
+end
+
 i = 0
 
 while true do
@@ -15,18 +19,27 @@ while true do
 
         if cmd == "forward" then
             turtle.forward()
+            resp()
         end
         
         if cmd == "back" then
             turtle.back()
+            resp()
         end
         
         if cmd == "left" then
             turtle.turnLeft()
+            resp()
         end
         
         if cmd == "right" then
             turtle.turnRight()
+            resp()
+        end
+
+        if cmd == "attack" then
+            turtle.attack()
+            resp()
         end
     end
 end
