f01a5bcf7b4464bdcd7a6144d01b03ebfabe2975
spv00 <sean.pa.voss@gmail.com>
2022-08-13 04:08:24 +0200

    ah yes

--- server/app.py
+++ server/app.py
@@ -31,8 +31,8 @@ def next_step():
 def resp():
     data = flask.request.get_data().strip().split(b",")
     index = int(data[0])
-    if index <= i:
-        next_step()
+    #if index <= i:
+    next_step()
     return str(data)
 
 @app.route("/setcmd/<newcmd>")
