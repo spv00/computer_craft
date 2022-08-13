098214085ed749ca1735e87105bad4b5cbbce571
spv00 <sean.pa.voss@gmail.com>
2022-08-13 02:06:57 +0200

    ah yes

--- server/app.py
+++ server/app.py
@@ -12,19 +12,9 @@ def increase():
     i += 1
     return str(i)
 
-@app.route("/controls", methods=['GET', 'POST'])
-def index():
-    if flask.request.method == 'POST':
-        if flask.request.form.get('action1') == 'VALUE1':
-            pass # do something
-        elif  flask.request.form.get('action2') == 'VALUE2':
-            pass # do something else
-        else:
-            pass # unknown
-    elif flask.request.method == 'GET':
-        return flask.render_template('controls.html', form=flask.form)
-    
-    return flask.render_template("index.html")
+@app.route("controls")
+def controls():
+    return flask.render_template("controls.html")
 
 @app.route("/index")
 def index():
